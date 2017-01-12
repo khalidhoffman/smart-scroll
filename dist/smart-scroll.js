f89a04284f496c1b9ab92f84696901409402e20a
Khalid Hoffman <khalidhoffman@gmail.com>
2017-01-12 14:51:26 -0500

    adds state for finer validation

--- dist/smart-scroll.js
+++ dist/smart-scroll.js
@@ -74,8 +74,9 @@ function BotScrollVerifier() {
     };
     this.$window = $(window);
     this.state = {
+        status: 'init',
+        timeoutId : -1,
         lastUserActionTimestamp: 0,
-        isOverriding: false,
         validationOverrideVal: false
     };
 
@@ -97,27 +98,74 @@ BotScrollVerifier.prototype = {
 
         this.$window.on(userEvents, self.updateTimeStamp);
 
-        // automatically invalidate all input if mouse is down
         this.$window.on('mousedown touchstart', function () {
-            self.state.isOverriding = true;
+            // automatically invalidate all input if mouse is down
+            self.setOverriding();
         });
         this.$window.on('mouseup', function () {
-            // lift override if mouse is lifted
-            self.state.isOverriding = false;
+            // continue listening once this mouse is lifted
+            // might have issues if user scrolls with 2 buttons pressed and lifts only 1
+            if (self.state.status == 'overriding') {
+                self.setActive();
+            }
         });
         this.$window.on('touchcancel touchend', function () {
             // use timeout to allow for slung page to settle
             setTimeout(function () {
-                self.state.isOverriding = false;
+                if (self.state.status == 'overriding'){
+                    self.setActive();
+                }
             }, self.config.flingScrollDelay)
         })
+    },
+
+    setInit: function(){
+        this.state.status = 'init';
+    },
+
+    setOverriding: function(){
+        this.state.status = 'overriding';
+    },
+
+    setActive: function(){
+        this.state.status = 'active';
+    },
+
+    setValidating: function(){
+        this.state.status = 'validating';
     },
 
     updateTimeStamp: function (val) {
         this.state.lastUserActionTimestamp = Date.now() || val;
     },
     validate: function () {
-        return this.state.isOverriding ? this.state.validationOverrideVal : Date.now() - this.state.lastUserActionTimestamp > this.config.verificationTimeLimit ? this.config.tag : false;
+        var self = this;
+
+        // debounce calls for reset
+        clearTimeout(this.state.timeoutId);
+        this.state.timeoutId = setTimeout(function(){
+            if (self.state.status != 'overriding'){
+                self.setInit();
+            }
+        }, this.verificationTimeLimit);
+
+        switch(this.state.status){
+            case 'overriding':
+                return this.state.validationOverrideVal;
+            case 'init':
+                this.state.prev = false;
+                this.setValidating();
+                setTimeout(function(){
+                    self.setActive();
+                }, this.config.verificationTimeLimit/2);
+            case 'validating':
+                return this.state.prev;
+            case 'active':
+                this.state.prev = Date.now() - this.state.lastUserActionTimestamp > this.config.verificationTimeLimit ? this.config.tag : false;
+                return this.state.prev;
+            default:
+                throw new Error('bot verifier has an invalid state: ' + self.state.status);
+        }
     }
 };
 
